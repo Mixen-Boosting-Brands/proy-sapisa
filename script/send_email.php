4ed8efd41ae556669a48cf814abb2ed8c260b421
Jonathan Nuñez <jonathan.ntorres@alumnos.udg.mx>
2025-05-18 20:53:43 -0600

    Change

--- script/send_email.php
+++ script/send_email.php
@@ -37,8 +37,8 @@ try {
     $mail->Host       = 'smtp.hostinger.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'jonathan.nunez@mixen.mx';                     //SMTP username
-    $mail->Password   = 'secret';                               //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Password   = 'G5wHWoJVYh>4';                               //SMTP password
+    $mail->SMTPSecure = 'ssl';            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
